f6fb6e8c0f82744a529c0bacdfe698b1560941ec
SEUONG WOO <andymon@naver.com>
2024-02-21 18:02:43 +0900

    20240221 수업내용

---
+++ Servlet2/src/main/java/edu/kh/servlet/practice/Pizza.java
@@ -0,0 +1,45 @@
+package edu.kh.servlet.practice;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Pizza extends HttpServlet{
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		
+		String pizza = req.getParameter("pizza");
+		String type = req.getParameter("type");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
